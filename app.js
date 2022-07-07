5372494c45ea7a5238846ab7c31169aa54efbb10
rosieett <58713682+rosieett@users.noreply.github.com>
2022-07-07 15:18:28 -0400

    scrolly overlay complete but still funky image width issues

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 // using d3 for convenience
 var main = d3.select("main");
-var scrolly = main.select("#scrolly");
-var figure = scrolly.select("figure");
-var article = scrolly.select("article");
-var image = scrolly.selectAll(".img")
-var step = article.selectAll(".step");
+
+//sideByside Config
+var scrolly_side = main.select(".scrolly_side");
+var figure_side = scrolly_side.select(".figure_sideBySide");
+var article_side = scrolly_side.select(".step_sideBySide");
+var image_side = scrolly_side.selectAll(".img_sideBySide")
+var step_side = article_side.selectAll(".step");
+
+//overlay config
+var scrolly_overlay = main.select(".scrolly_overlay");
+var figure_overlay = scrolly_overlay.select(".figure_overlay");
+var article_overlay = scrolly_overlay.select(".step_overlay");
+var image_overlay = scrolly_overlay.selectAll(".img_overlay")
+var step_overlay = article_overlay.selectAll(".step_center");
+
+
 
 // initialize the scrollama
 var scroller = scrollama();
 
 // generic window resize listener event
-function handleResize() {
+function handleResizeSidebySide() {
 
     // 2. update the height of the figure
     var figureHeight = window.innerHeight * .8;
     var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
-    figure
+    figure_side
         .style("height", figureHeight + "px")
         .style("top", figureMarginTop + "px")
         .style("margin-bottom", figureMarginTop + "px")
@@ -25,19 +36,28 @@ function handleResize() {
     scroller.resize();
 }
 
+function handleResizeOverlay() {
+    // 2. update the height of the figure
+    var figureHeight = window.innerHeight
+    var figureMarginTop = 0
+    var articleMarginButton = window.innerHeight * .02
+    console.log(window.innerHeight, figureHeight * 1)
+
+    figure_overlay
+        .style("height", figureHeight + "px")
+        .style("top", figureMarginTop + "px")
+
+    article_overlay
+        .style("margin-bottom", articleMarginButton + "px")
+
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+}
+
 // scrollama event handlers
 function handleStepEnter(response) {
-    console.log(response);
-    // response = { element, direction, index }
-
-    //changing colors of active step 
-    // step.classed("is-active", function (d, i) {
-    //     return i === response.index;
-    // });
-
     //adding in active class to
     document.getElementById(response.element.dataset.target).classList.add('active');
-
 }
 
 function reset(response) {
@@ -46,17 +66,14 @@ function reset(response) {
     }
 }
 
-function init() {
+function initSideBySide() {
 
     // 1. force a resize on load to ensure proper dimensions are sent to scrollama
-    handleResize();
+    handleResizeSidebySide();
 
-    // 2. setup the scroller passing options
-    // 		this will also initialize trigger observations
-    // 3. bind scrollama event handlers (this can be chained like below)
     scroller
         .setup({
-            step: "#scrolly article .step",
+            step: ".scrolly_side .step_sideBySide .step",
             offset: 0.5,
             debug: false
         })
@@ -64,5 +81,29 @@ function init() {
         .onStepExit(reset)
 }
 
-// kick things off
-init();
+function initOverlay() {
+
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResizeOverlay();
+
+    scroller
+        .setup({
+            step: ".scrolly_overlay .step_overlay .step_center",
+            offset: 0.5,
+            debug: false
+        })
+        .onStepEnter(handleStepEnter)
+        .onStepExit(reset)
+
+}
+
+// var page = document.getElementById(body);
+
+// function handleForm(event) {
+//     event.preventDefault();
+// }
+// body.addEventListener('scroll', handleForm);
+
+
+initSideBySide();
+initOverlay();
